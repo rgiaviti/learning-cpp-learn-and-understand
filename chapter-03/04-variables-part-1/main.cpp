83de443b46065ed4e9d83c33ea19a01238043880
Ricardo Giaviti <rgiaviti@gmail.com>
2022-02-24 13:03:05 -0300

    Variables Part 1-2

--- chapter-03/04-variables-part-1/main.cpp
+++ chapter-03/04-variables-part-1/main.cpp
@@ -5,6 +5,11 @@ using namespace std;
 // Take a look on README.md to see all C++ Fundamental Types
 int main() {
     // Declaring different types of vars and assigning
-    std::cout << sum(4, 6) << std::endl;
+    int     a = 5;
+    float   b = 3.14;
+    double  c = 3.1541221;
+    char    d = 'A';
+    wchar_t e = 'B';
+    bool    f = true;
     return 0;
 }
