5b2f48b0eedfbb5d273cda92e2660dcbd2292dfe
Ricardo Giaviti <rgiaviti@gmail.com>
2022-03-07 14:11:55 -0300

    Chapter 5 - Lesson 1

---
+++ chapter-05/01-include-other-libraries/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream> //a library
+#include <string> //another library
+
+// #include <time.h> // c format extension .h is valid in c++ but not necessary
+// #include <ctime> //c++ format
+
+// Example C++ syntax and C syntax
+// C Syntax: #include <my_library.h> --> C++ Syntax: #include <cmy_library>
+
+// Using path
+// #include "some/path/to/a/library.h"
+
+#include <stdlib.h>
+
+int main() {
+    printf("Hello World"); //using C function presented in stdlib
+}
