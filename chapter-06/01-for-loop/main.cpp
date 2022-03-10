53d5df7a443a06ac3e3a8bb37c00a8043fba2d0e
Ricardo Giaviti <rgiaviti@gmail.com>
2022-03-10 16:58:39 -0300

    Studying Chapter 06

--- chapter-06/01-for-loop/main.cpp
+++ chapter-06/01-for-loop/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
-int main() {
+int main()
+{
+
+    int n;
+    std::cout << "Enter n: ";
+    std::cin >> n;
+
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int number;
+        std::cout << "Enter " << i << ". number: ";
+        std::cin >> number;
+        sum = sum + number;
+    }
+
+    std::cout << "The Sum of Number is: " << sum << std::endl;
+
     return 0;
 }
