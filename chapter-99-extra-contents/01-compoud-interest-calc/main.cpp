7b0341b4d76f6683a94e5b3711e0e615afdc02c4
Ricardo Giaviti <rgiaviti@gmail.com>
2022-03-16 15:25:34 -0300

    Extra Contents!

--- chapter-99-extra-contents/01-compoud-interest-calc/main.cpp
+++ chapter-99-extra-contents/01-compoud-interest-calc/main.cpp
@@ -6,23 +6,51 @@ void calculateStepByStep(float, float, float, float);
 
 int main()
 {
-    float initialValue = 1000.00f;         // initial value
-    float interestRate = 50.0f;            // in percent: example = 100 for 100%
-    float time = 12.0f;                    // days, months, or years... whatever
-    float recurrencyContribution = 10.00f; // contribution
-    int calculationType = 1;               // 0 = Calculate By Formula, 1 = Calculate Step by Step
+    float initialValue;           // initial value
+    float interestRate;           // in percent: example = 100 for 100%
+    float time;                   // days, months, or years... whatever
+    float recurrencyContribution; // contribution
+    unsigned int calculationType;
+
+    std::cout << "Initial Money: ";
+    std::cin >> initialValue;
+
+    std::cout << "Interest Rate: ";
+    std::cin >> interestRate;
+
+    std::cout << "Time (days, months or years): ";
+    std::cin >> time;
+
+    do
+    {
+        std::cout << "Calculation Type (1 = By Formula, 2 = Step By Step): ";
+        std::cin >> calculationType;
+
+        if (calculationType != 1 && calculationType != 2)
+        {
+            std::cout << "Incorrect Calculation Type. Try Again...";
+        }
+
+        if (calculationType == 2)
+        {
+            std::cout << "Recurrency Contribution: ";
+            std::cin >> recurrencyContribution;
+        }
+    } while (calculationType != 1 && calculationType != 2);
 
     std::cout.precision(2);
     std::cout << "Initial Value: " << std::fixed << initialValue << std::endl;
     std::cout << "Interest Rate: " << std::fixed << interestRate << "%" << std::endl;
     std::cout << "Time: " << std::fixed << time << " (Days, Months or Years)" << std::endl;
 
-    if (calculationType == 0) {
+    if (calculationType == 1)
+    {
         calculateByFormula(initialValue, interestRate, time);
         std::cout << std::endl;
     }
 
-    if (calculationType == 1) {
+    if (calculationType == 2)
+    {
         std::cout << "Contribution: " << std::fixed << recurrencyContribution << std::endl;
         std::cout << std::endl;
         calculateStepByStep(initialValue, interestRate, time, recurrencyContribution);
