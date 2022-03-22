1fc943c32b2c29d78200d9e9fa6866a34ff1c573
Ricardo Giaviti <rgiaviti@gmail.com>
2022-03-22 18:41:52 -0300

    Studying Exception Handling

--- chapter-09-exception-handling/01-explanation-and-usage-part-1/main.cpp
+++ chapter-09-exception-handling/01-explanation-and-usage-part-1/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <cmath>
 
+double Sqrt(double x)
+{
+    double sroot = std::pow(x, 0.5);
+    return sroot;
+}
+
 int main()
 {
+    double num;
+
+    std::cout << "Enter number: ";
+    std::cin >> num;
+
+    double square_root = Sqrt(num);
+    std::cout << square_root << std::endl;
+
     return 0;
 }
--- chapter-09-exception-handling/02-explanation-and-usage-part-2/main.cpp
+++ chapter-09-exception-handling/02-explanation-and-usage-part-2/main.cpp
@@ -1,6 +1,74 @@
 #include <iostream>
+#include <cmath>
+#include <stdexcept>
+
+double Sqrt(double x)
+{
+    if (x < 0)
+    {
+        throw "Negative Number";
+    }
+
+    double sroot = std::pow(x, 0.5);
+    return sroot;
+}
+
+void something(int n)
+{
+    if (n == 0)
+    {
+        throw 1;
+    }
+
+    if (n > 0 && n < 100)
+    {
+        throw "bla bla bla";
+    }
+
+    if (n == -1)
+    {
+        throw 'c';
+    }
+
+    if (n < -1)
+    {
+        throw std::domain_error("some exception");
+    }
+}
 
 int main()
 {
+    double num;
+
+    std::cout << "Enter number: ";
+    std::cin >> num;
+
+    try
+    {
+        // double square_root = Sqrt(num);
+        something(num);
+        // std::cout << square_root << std::endl;
+    }
+    catch (const char *text)
+    {
+        std::cout << "Catch Pointer Char: " << text << std::endl;
+        return 1;
+    }
+    catch (const int e)
+    {
+        std::cout << "Catch Int: " << e << std::endl;
+        return 1;
+    }
+    catch (const char c)
+    {
+        std::cout << "Catch Char: " << c << std::endl;
+        return 1;
+    }
+    catch (std::domain_error *e)
+    {
+        std::cout << "Domain Error: " << e->what() << std::endl;
+        return 1;
+    }
+
     return 0;
 }
