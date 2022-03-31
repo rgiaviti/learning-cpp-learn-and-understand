43fa6b02c5fffc83930b17f831538c2d07a58df6
Ricardo Giaviti <rgiaviti@gmail.com>
2022-03-31 19:59:16 -0300

    Continuing Chapter 14

--- chapter-14-classes-in-cpp-basics/01-rule-zero-three-five-constructors-part-1/main.cpp
+++ chapter-14-classes-in-cpp-basics/01-rule-zero-three-five-constructors-part-1/main.cpp
@@ -2,15 +2,35 @@
 
 class DummyClass
 {
+
+    int dummy_attr1;
+    char dummy_attr2;
+
 public:
-    DummyClass()
+    DummyClass() : dummy_attr1(0), dummy_attr2('E')
     {
         std::cout << "Hello! Constructor Here!" << std::endl;
+    }
+
+    DummyClass(int param) : dummy_attr1(1), dummy_attr2('P')
+    {
+        std::cout << "2nd constructor - " << param << std::endl;
+    }
+
+    void Print() const
+    {
+        std::cout << "dummy_attr1 = " << this->dummy_attr1 << std::endl;
+        std::cout << "dummy_attr2 = " << this->dummy_attr2 << std::endl;
     }
 };
 
 int main()
 {
     DummyClass dummy; // call the constructor
+    dummy.Print();
+
+    DummyClass dummy2(7); // call the constructor with parameter
+    dummy2.Print();
+
     return 0;
 }
