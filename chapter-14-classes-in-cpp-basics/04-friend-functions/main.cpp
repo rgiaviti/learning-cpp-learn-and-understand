fe411d341dbdccafdad31c6ad88797a2cf939100
Ricardo Giaviti <rgiaviti@gmail.com>
2022-04-04 16:30:09 -0300

    Finished Chapter 14

--- chapter-14-classes-in-cpp-basics/04-friend-functions/main.cpp
+++ chapter-14-classes-in-cpp-basics/04-friend-functions/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 
+class DummyClass
+{
+private:
+    int dummy_attr1;
+    char dummy_attr2;
+
+public:
+    DummyClass() : dummy_attr1(0), dummy_attr2('E')
+    {
+        std::cout << "Hello! Constructor Here!" << std::endl;
+    }
+
+    DummyClass(int param) : dummy_attr1(1), dummy_attr2('P')
+    {
+        this->dummy_attr1 = param;
+        std::cout << "2nd constructor - " << param << std::endl;
+    }
+
+    void Print() const
+    {
+        std::cout << "dummy_attr1 = " << this->dummy_attr1 << std::endl;
+        std::cout << "dummy_attr2 = " << this->dummy_attr2 << std::endl;
+    }
+
+    friend int Add(DummyClass dc1, DummyClass dc2)
+    {
+        return dc1.dummy_attr1 + dc2.dummy_attr1;
+    }
+
+    ~DummyClass()
+    {
+        std::cout << "Destructor Called" << std::endl;
+    }
+};
+
+
 int main()
 {
+    DummyClass dc1(2), dc2(3);
+    std::cout << Add(dc1, dc2) << std::endl;
     return 0;
 }
--- chapter-14-classes-in-cpp-basics/05-generic-classes/main.cpp
+++ chapter-14-classes-in-cpp-basics/05-generic-classes/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+template <typename TYPE>
+class DummyGeneric
+{
+    TYPE attribute;
+
+public:
+    DummyGeneric(TYPE param) : attribute(param)
+    {
+    }
+
+    TYPE GetAttribute() const
+    {
+        return this->attribute;
+    }
+
+    void SetAttribute(TYPE a)
+    {
+        this->attribute = a;
+    }
+};
+
 int main()
 {
+    DummyGeneric<std::string> dg1("Hey");
+    DummyGeneric<int> dg2(12);
+    DummyGeneric<char> dg3('s');
+
     return 0;
 }
