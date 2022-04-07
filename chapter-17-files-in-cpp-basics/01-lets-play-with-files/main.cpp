008b98e3e92c0454aa87ce62639a9c28460a5fb6
Ricardo Giaviti <rgiaviti@gmail.com>
2022-04-07 19:38:33 -0300

    Finished Chapter 17

--- chapter-17-files-in-cpp-basics/01-lets-play-with-files/main.cpp
+++ chapter-17-files-in-cpp-basics/01-lets-play-with-files/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
+    std::string s;
+    std::cout << "Enter some text: ";
+    std::getline(std::cin, s);
+
+    int n;
+
+    std::cout << "Enter number n: ";
+    std::cin >> n;
+
+    std::ofstream f("test.txt");
+    if (!f)
+    {
+        std::cout << "File does not exists";
+        return 1;
+    }
+
+    for (int i = 1; i <= n; ++i)
+    {
+        f << s << std::endl;
+    }
+
+    f.close();
+
+    return 0;
 }
