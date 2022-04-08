15664837997391f474b43e310e56c4a44e79dc58
Ricardo Giaviti <rgiaviti@gmail.com>
2022-04-08 16:14:55 -0300

    Finished Chapter 18

--- chapter-18-basic-encryption/01-some-fun-with-basic-encryption/main.cpp
+++ chapter-18-basic-encryption/01-some-fun-with-basic-encryption/main.cpp
@@ -1,5 +1,59 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+std::string ROT13(std::string s);
 
 int main()
 {
+    std::string text, s, file_path;
+    std::cout << "Enter test (--- in newline to end)" << std::endl;
+
+    while (std::getline(std::cin, s))
+    {
+        if (s != "---")
+        {
+            text += s + "\n";
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    std::cout << "Enter name of the file: ";
+    std::getline(std::cin, file_path);
+    std::ofstream file(file_path);
+    
+    if (!file)
+    {
+        std::cout << "Error! Cannot write to" << file_path << std::endl;
+        return 1;
+    }
+
+    file << ROT13(text);
+    file.close();
+    
+    return 0;
+    
+}
+
+std::string ROT13(std::string s)
+{
+    for (char &c : s)
+    {
+        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+        {
+            if (c < 'a' + 13 || c < 'A' + 13)
+            {
+                c = c + 13;
+            }
+            else
+            {
+                c = c - 13;
+            }
+        }
+    }
+
+    return s;
 }
